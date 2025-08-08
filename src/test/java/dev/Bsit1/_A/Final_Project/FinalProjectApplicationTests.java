908f565cc6756d3031eeecc855d2e62fb25abfed
keen1234 <kenthcabanero252@gmail.com>
2025-08-09 00:09:15 +0800

    test

---
+++ src/test/java/dev/Bsit1/_A/Final_Project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Bsit1._A.Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
